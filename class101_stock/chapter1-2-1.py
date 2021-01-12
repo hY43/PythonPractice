ece3062729e54310beb0b2d4f3adceb15d532597
hY43 <hyde0401@gmail.com>
2021-01-13 07:45:47 +0900

    print, valuable

--- class101_stock/chapter1-2-1.py
+++ class101_stock/chapter1-2-1.py
@@ -1,1 +1,18 @@
-print("hello")
+# String 출력
+print("삼성전자")
+
+# 정수형 int 출력
+print(5560)
+
+# 실수형 float 출력
+print(0.123)
+
+# 참/거짓 boolean 출력
+print(True)
+print(False)
+
+# 섞어서 사용하기
+print("종목명 : %s" % "삼성전자")
+print("종목명 : %s, 종목코드 : %s, 현재가 : %f" % ("삼성전자", "005930", 5600.12))
+
+# 시가, 저가, 고가, 종가
---
+++ class101_stock/chapter1-2-2.py
@@ -0,0 +1,19 @@
+# 변수 활용
+stockName = None
+stockName = "삼성전자"
+print("종목명 : %s" % stockName)
+print("변수 타입 : %s" % type(stockName))
+
+stockPrice = 56000
+print("현재가 : %i" % stockPrice)
+print("변수 타입 : %s" % type(stockPrice))
+
+stockRate = -0.72
+print("등락율 : %f" % stockRate)
+print("등락율 : %f%%" % stockRate)
+print("등락율 : %f%s" % (stockRate, "%"))
+print("변수 타입 : %s" % type(stockRate))
+
+mesuOk = False
+print("매수여부 : %s" % mesuOk)
+print("변수 타입 : %s" % type(mesuOk))
